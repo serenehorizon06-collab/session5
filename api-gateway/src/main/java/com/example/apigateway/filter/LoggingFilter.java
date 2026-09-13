5a07b1de8eff840e8757a187eb729b5138e6f572
HuyDung <nguyenhdung2006@gmail.com>
2026-09-13 10:58:06 +0700

    Bai 3

---
+++ api-gateway/src/main/java/com/example/apigateway/filter/LoggingFilter.java
@@ -0,0 +1,23 @@
+package com.example.apigateway.filter;
+
+import java.time.LocalDateTime;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println(LocalDateTime.now() + " Incoming request to: " + exchange.getRequest().getURI());
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -2;
+    }
+}
